b05c1a00e7fcc3a5c9de4174df8401fe441cefbc
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-01-30 18:19:04 +0900

    renameコマンドの実装

---
+++ src/commands/rename.ts
@@ -0,0 +1,22 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { Args } from '@sapphire/framework';
+import { SubCommandPluginCommand, SubCommandPluginCommandOptions } from '@sapphire/plugin-subcommands';
+import type { Message } from 'discord.js';
+
+@ApplyOptions<SubCommandPluginCommandOptions>({
+    description: 'botの名前を変更します。',
+    options: ['name']
+})
+export class UserCommand extends SubCommandPluginCommand {
+	public async messageRun(message: Message, args: Args) {
+        const oldName = message.guild?.me?.displayName
+        const newName = args.getOption('name')
+        if (newName == null) {
+            return message.reply('botの名前が指定されていません。\n`ab.rename --name=[新しい名前]`と入力してください。')
+        } else if (newName.length >= 27) {
+            return message.reply('botの名前は27文字以内にしてください。')
+        }
+        await message.guild?.me?.setNickname(newName + "[ab.]")
+        return message.reply(`botの名前を${oldName}から${newName}に変更しました。`)
+	}
+}
