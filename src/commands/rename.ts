c1b616800773e3464604f874ec1c039447d0bb85
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-01-31 10:15:03 +0900

    renameコマンドの変更
    個人鯖以外でも運用するため，アクティビティに前の名前をセットしないように変更

--- src/commands/rename.ts
+++ src/commands/rename.ts
@@ -4,19 +4,18 @@ import { SubCommandPluginCommand, SubCommandPluginCommandOptions } from '@sapphi
 import type { Message } from 'discord.js';
 
 @ApplyOptions<SubCommandPluginCommandOptions>({
-    description: 'botの名前を変更します。',
-    options: ['name']
+	description: 'botの名前を変更します。',
+	options: ['name']
 })
 export class UserCommand extends SubCommandPluginCommand {
 	public async messageRun(message: Message, args: Args) {
-        const oldName = message.guild?.me?.displayName
-        const newName = args.getOption('name')
-        if (newName == null) {
-            return message.reply('botの名前が指定されていません。\n`ab.rename --name=[新しい名前]`と入力してください。')
-        } else if (newName.length >= 27) {
-            return message.reply('botの名前は27文字以内にしてください。')
-        }
-        await message.guild?.me?.setNickname(newName + "[ab.]")
-        return message.reply(`botの名前を${oldName}から${newName}に変更しました。`)
+		const oldName = message.guild?.me?.displayName;
+		const newName = args.getOption('name');
+		if (newName == null) {
+			return message.reply('botの名前が指定されていません。\n`ab.rename --name=[新しい名前]`と入力してください。');
+		} else if (newName.length >= 27) {
+			return message.reply('botの名前は27文字以内にしてください。');
+		}
+		return message.reply(`botの名前を${oldName}から${newName}に変更しました。`);
 	}
 }
